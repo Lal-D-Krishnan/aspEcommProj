b3a9c2c532302f370f331586ad1a96f8f254a75c
Lal Krishnan <krishnanpraveen099@gmail.com>
2023-10-03 16:24:27 +0530

    readonly removed in ApplicationUserRepository

--- BulkyWeb/Bulky.DataAccess/Repository/ApplicationUserRepository.cs
+++ BulkyWeb/Bulky.DataAccess/Repository/ApplicationUserRepository.cs
@@ -12,7 +12,7 @@ namespace Bulky.DataAccess.Repository
 {
     public class ApplicationUserRepository : Repository<ApplicationUserRepository>, IApplicationUserRepository
     {
-        private readonly ApplicationDbContext _db;
+        private  ApplicationDbContext _db;
         public ApplicationUserRepository(ApplicationDbContext db) : base(db) 
         {
             _db = db;        
